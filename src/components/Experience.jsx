a90720c358322688fa43e93bb8c3a86e4f4d3cfb
Ridhi Rajput <ridhirajput333@gmail.com>
2024-10-11 13:28:27 +0530

    Aligning the Experience Section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,9 +4,9 @@ const Experience = () => {
     return (
         <div className = "border-b border-neutral-900 pb-4">
             <h2 className = "my-20 text-center text-4xl">Experience</h2>
-            <div>
+            <div className = "my-20 text-center">
                 {EXPERIENCES.map((experience, index) => (
-                    <div key = {index} className = "mb-8 flex flex-wrap lg:justify-center">
+                    <div key = {index} className = "mx-5 mb-8 flex flex-wrap lg:justify-center">
                         <div className = "w-full lg:w-1/4">
                         <p className = "mb-2 text-sm text-neutral-400">{experience.year}</p>
                         </div>
