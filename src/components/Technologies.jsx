9acb82a1a434b8740a0a987ba2c985b548a0dbbf
Ridhi Rajput <ridhirajput333@gmail.com>
2024-10-11 12:25:51 +0530

    new commit

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,3 +1,4 @@
+//importing icons from react icons
 import {RiReactjsLine} from "react-icons/ri";
 import {TbBrandNextjs} from "react-icons/tb";
 import {SiMongodb} from "react-icons/si";
@@ -10,23 +11,24 @@ const Technologies = () => {
         <div className = "border-b border-neutral-800 pb-24">
             <h2 className = "my-20 text-center text-4xl">Technologies</h2>
             <div className = "flex flex-wrap items-center justify-center gap-4">
+                
                 <div className = "rounded-2xl border-4 border-neutral-800 p-4">
                     <RiReactjsLine className = "text-7xl text-cyan-400" />
                 </div>
                 <div className = "rounded-2xl border-4 border-neutral-800 p-4">
-                    <TbBrandNextjs className = "text-7xl text-cyan-400" />
+                    <TbBrandNextjs className = "text-7xl" />
                 </div>
                 <div className = "rounded-2xl border-4 border-neutral-800 p-4">
-                    <SiMongodb className = "text-7xl text-cyan-400" />
+                    <SiMongodb className = "text-7xl text-green-500" />
                 </div>
                 <div className = "rounded-2xl border-4 border-neutral-800 p-4">
-                    <DiRedis className = "text-7xl text-cyan-400" />
+                    <DiRedis className = "text-7xl text-red-700" />
                 </div>
                 <div className = "rounded-2xl border-4 border-neutral-800 p-4">
-                    <FaNodeJs className = "text-7xl text-cyan-400" />
+                    <FaNodeJs className = "text-7xl text-green-500" />
                 </div>
                 <div className = "rounded-2xl border-4 border-neutral-800 p-4">
-                    <BiLogoPostgresql className = "text-7xl text-cyan-400" />
+                    <BiLogoPostgresql className = "text-7xl text-sky-700" />
                 </div>
             </div>
         </div>
