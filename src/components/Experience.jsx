3a863ed5d50359120bbb05e16f9742655f8aa404
Ridhi Rajput <ridhirajput333@gmail.com>
2024-10-14 18:26:06 +0530

    commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,7 @@ const Experience = () => {
     return (
         <div className = "border-b border-neutral-900 pb-4">
             <h2 className = "my-20 text-center text-4xl">Experience</h2>
-            <div className = "my-20 text-center">
+            <div>
                 {EXPERIENCES.map((experience, index) => (
                     <div key = {index} className = "mx-5 mb-8 flex flex-wrap lg:justify-center">
                         <div className = "w-full lg:w-1/4">
