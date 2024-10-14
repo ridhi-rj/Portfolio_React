01be78fd70960d696d18f399b2d8d9b9cd5ccb35
Ridhi Rajput <ridhirajput333@gmail.com>
2024-10-14 18:26:27 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
             <div className = "flex flex-wrap">
                 <div className = "w-full lg:w-1/2">
                 <div className = "flex flex-col lg:items-start">
-                    <h1 className = "pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text8xl">Ridhi Rajput</h1>
+                    <h1 className = "pb-10 text-6xl font-thin tracking-tight lg:mt-16 lg:text8xl">Ridhi Rajput</h1>
                     <span className = "bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer
                     </span>
                     <p className = "text-justify my-2 max-w-xl py-6 font-light tracking-tighter">
@@ -17,7 +17,7 @@ const Hero = () => {
              </div>
              <div className = "w-full lg:w-1/2 p-8">
                 <div className = "flex justify center">
-                    <img className = "rounded" src = {profilePic} alt="Ridhi Rajput"></img>
+                    <img className = "rounded w-full sm:w-48 md:w-64 lg:w-96 h-auto" src = {profilePic} alt="Ridhi Rajput"></img>
                 </div>
              </div>
         </div>
